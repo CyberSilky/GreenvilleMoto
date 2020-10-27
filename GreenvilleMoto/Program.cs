416bd075031f91e0a6e7f71098df29550c1968e9
James Shoemaker <james.shoemaker99@gmail.com>
2020-10-27 13:50:02 -0500

    Add project files.

---
+++ GreenvilleMoto/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+//Name: James Shoemaker
+//Date: 03/19/2020
+//Program Description: Assignment 1 week 1 for C#
+
+namespace GreenvilleMoto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**********************************");
+            Console.WriteLine("* The stars shine in Greenville. *");
+            Console.WriteLine("**********************************");
+               
+        }
+    }
+}
